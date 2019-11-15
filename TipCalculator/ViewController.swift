f645d4375aa48f264f35a27bed5bf01c42084d18
Peter Akala <47007577+petecsn@users.noreply.github.com>
2019-11-15 11:54:38 -0800

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by peter on 11/15/19.
+//  Copyright © 2019 peter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
