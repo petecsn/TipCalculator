06771acbc3dd1683d53674ca39c5dd72e44f8ced
Peter Akala <47007577+petecsn@users.noreply.github.com>
2019-11-15 19:23:44 -0800

    added logic & event handler
    -added event handler to bill amount text field
    -added logic to calculate tip and total

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,5 +25,33 @@ class ViewController: UIViewController {
         view.endEditing(true)
     }
     
+    @IBAction func calculateTip(_ sender: Any) {
+        removeDoubleDecimalPoint()
+        
+        // get bill amount input
+        let bill = Double(billField.text!) ?? 0
+        
+        // calculate tip and total
+        let tipPercentage = getTipPercentage()
+        let tip = bill * tipPercentage
+        let total = bill + tip
+        
+        // update tip and total labels
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    func getTipPercentage() -> Double {
+        let tipPercentages = [0.15, 0.18, 0.20]
+        return tipPercentages[tipControl.selectedSegmentIndex]
+    }
+    
+    func removeDoubleDecimalPoint() {
+        let bill = billField.text!
+        
+        if (bill.contains("..")) {
+            billField.text = bill.replacingOccurrences(of: "..", with: ".")
+        }
+    }
 }
 
